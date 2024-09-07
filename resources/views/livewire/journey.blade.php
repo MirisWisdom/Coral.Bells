be982beb77f13b889197b0fdaf9bd5c612d99113
Emilian Roman <git@miris.design>
2024-09-08 02:57:21 +0800

    modal + guidance example
    Signed-off-by: Emilian Roman <git@miris.design>

--- resources/views/livewire/journey.blade.php
+++ resources/views/livewire/journey.blade.php
@@ -40,10 +40,61 @@
                 </div>
             </div>
         </div>
-        <button class="btn btn-primary btn-lg text-uppercase text-light w-100 mt-4 py-3">
+        <button class="btn btn-primary btn-lg text-uppercase text-light w-100 mt-4 py-3"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleGuidance">
             <div class="h3 mb-0">
                 Safely Guide Me
             </div>
         </button>
+
+        <!-- Modal -->
+        <div class="modal fade"
+             id="exampleGuidance"
+             tabindex="-1"
+             aria-labelledby="exampleGuidanceLabel"
+             aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title"
+                            id="exampleGuidanceLabel">
+                            Our Recommendations
+                        </h5>
+                        <button type="button"
+                                class="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Other corals have raised a few bells in the area you are visiting, particularly regarding cases
+                        of crocodiles eating the tires of cars going at high speeds.
+                        <br><br>
+                        We recommend taking the road to the right. It may take a bit longer to arrive, but it is near a
+                        populated area with lots of support
+                        and facilities available for you.
+                        <br><br>
+                        <div class="guidance img-thumbnail"></div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="row">
+                            <div class="col">
+                                <button type="button"
+                                        class="btn btn-secondary text-dark">
+                                    Give me alternatives...
+                                </button>
+                            </div>
+                            <div class="col-auto">
+                                <button type="button"
+                                        class="btn btn-secondary text-dark"
+                                        data-bs-dismiss="modal">
+                                    Understood!
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
