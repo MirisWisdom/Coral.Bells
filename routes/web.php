46fdbe5e63dcd37bb8f3296cabfd8916adc9fa1f
Emilian Roman <git@miris.design>
2024-09-07 22:09:27 +0800

    wip
    Signed-off-by: Emilian Roman <git@miris.design>

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/report', function () {
+    return view('report');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
